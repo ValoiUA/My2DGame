c5c626fa516cc26f4a966f195d282c9529da80cc
ValoiUA <141871360+ValoiUA@users.noreply.github.com>
2024-12-25 17:36:02 +0200

    first try

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,7 +1,5 @@
 package main;
 
-import entity.Player;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -20,13 +18,9 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenHeight = tileSize * maxScreenRow;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-
     int playerX = 100;
     int playerY = 100;
     int playerSpeed = 4;
-
-    Player player = new Player(this, keyH);
-
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -51,9 +45,6 @@ public class GamePanel extends JPanel implements Runnable {
             update();
 
             repaint();
-
-
-
             try {
                 double remaingingTime = nextDrawTime - System.nanoTime();
                 remaingingTime = remaingingTime/1000000;
@@ -70,7 +61,15 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
     public void update() {
-
+        if (keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }else if (keyH.downPressed == true){
+            playerY += playerSpeed;
+        }else if (keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }else if (keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
     }
     public void paintComponent(Graphics g) {
 
@@ -78,8 +77,9 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D)g;
 
+        g2.setColor(Color.white);
 
-
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
