485452edfa865737840077abf86d16678cf8da3c
ValoiUA <141871360+ValoiUA@users.noreply.github.com>
2024-12-25 13:25:15 +0200

    Main java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2DGame");
 
+
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
